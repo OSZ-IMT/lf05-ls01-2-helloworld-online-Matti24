092cd9568f8dc305c1bfa18795a85a05ffbf54cf
Matti24 <matti@im-dickicht.de>
2020-09-09 16:22:34 +0200

    Hausaufgabe: Inhalt von Hello World geändert

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Guten Tag");
+        System.out.println("Willkommen in der Veranstaltung \"Strukturierte Programmierung\".");
     }
 }
